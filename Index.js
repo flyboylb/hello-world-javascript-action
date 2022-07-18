b06e32c3d7d4b869ee5db6e1063dd9964ca3256c
Patrick Delifer <pdelifer@gmail.com>
2022-07-18 17:08:39 -0400

    sdfg

--- Index.js
+++ Index.js
@@ -8,7 +8,7 @@ async function run() {
         let prerelease = getInput("prerelease", { required: false });
         let mine = "";
         let tagprefix = getInput("buildtagprefix", { required: true });
-
+        console.log(`tagprefix is ${tagprefix}`);
         let currentVersionTag = await getCurrentTag();
 
         if (currentVersionTag) {
@@ -18,7 +18,7 @@ async function run() {
         }
 
         let nextVersion = await getNextVersionTag(tagprefix,{ prerelease });
-        
+        console.log(`nextv is ${nextVersion}`);
         core.exportVariable("release_tag", nextVersion);
       
     } catch (error) {
@@ -57,8 +57,9 @@ function getNextDateVersion(tagprefix, previousVersionTags) {
     while (_tagExists(newVersionParts, previousVersionTags)) {
         newVersionParts[3]++;
     }
-
-    return string.concat(tagprefix,newVersionParts.join("."));
+    let versionnumber = newVersionParts.join(".");
+    let outputvar = `${versionnumber}-${versionnumber}`;
+    return outputvar;
 }
 
 function getPrereleaseVersion(previousVersionTags, prerelease) {

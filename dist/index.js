927014c6faf45a881e36311444bd08ba1256bea2
Patrick Delifer <pdelifer@gmail.com>
2022-07-18 14:33:08 -0400

    update getlatest

--- dist/index.js
+++ dist/index.js
@@ -9014,12 +9014,48 @@ try {
 
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+  getCurrentTag();
+
 }
 catch (error)
 {
   core.setFailed(error.message);
+}
+
+ function getCurrentTag() {
+    exec("git fetch --tags");
+
+    // First Check if there is already a release tag at the head...
+    let currentTags = execGetOutput(`git tag --points-at ${context.sha}`);
+    console.log(`Current Tags: ${currentTags}`);
+    return currentTags.map(processVersion).filter(Boolean)[0];
+}
+
+function execGetOutput(command) {
+    let collectedOutput = [];
+    let collectedErrorOutput = [];
+
+    let options = {
+        listeners: {
+            stdout: (data) => {
+                let output = data.toString().split("\n");
+                collectedOutput = collectedOutput.concat(output);
+            },
+            stderr: (data) => {
+                let output = data.toString().split("\n");
+                collectedErrorOutput = collectedErrorOutput.concat(output);
+            },
+        },
+    };
+
+    try {
+         exec(command, [], options);
+    } catch (error) {
+        throw new Error(collectedErrorOutput);
+    }
+
+    return collectedOutput;
 }
 })();
 
